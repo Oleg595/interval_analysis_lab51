cae95e64cf0d705ab3304eb05aa09bae8edfca23
Oleg595 <60892746+Oleg595@users.noreply.github.com>
2022-01-10 12:29:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as pth
 from numpy.linalg import lstsq
-import sympy as sm
+import numpy as np
 
 N = 10
 
 x = [i for i in range(1, N + 1)]
-y = [399, 646, 1059, 1472, 1692, 2078, 2491, 2904, 3316, 3729]
+y = [200 * i + int(5 * i * np.random.normal(size=1)) for i in range(1, N + 1)]
+for elem in y:
+    print(elem)
 
 plt.scatter(x, y)
 plt.xlabel("Номер измерения")
@@ -23,7 +25,7 @@ print(b1, b0)
 
 y_calc = [b1 * x[i] + b0 for i in range(N)]
 plt.plot(x, y_calc)
-plt.legend(["Результат МНК метода"])
+plt.legend(["Результат МНР метода"])
 plt.scatter(x, y)
 plt.xlabel("Номер измерения")
 plt.ylabel("Показания энкодера")
@@ -32,10 +34,11 @@ plt.show()
 eps = [math.fabs(y_calc[i] - y[i]) for i in range(N)]
 intervals = [[y[i] - eps[i], y[i] + eps[i]] for i in range(N)]
 
+
 def plotIntervals(intervals, x, y_calc):
     fig, ax = plt.subplots()
     ax.plot(x, y_calc, linewidth=0.5)
-    ax.legend(["Значение МНК"])
+    ax.legend(["Значение МНР"])
     for i in range(len(intervals)):
         ax.add_patch(
             pth.Rectangle((x[i], intervals[i][0]), 0, (intervals[i][1] - intervals[i][0]),
@@ -44,6 +47,7 @@ def plotIntervals(intervals, x, y_calc):
     plt.xlabel("Номер измерения")
     plt.ylabel("Приближённое показание энкодера")
     plt.show()
+
 
 plotIntervals(intervals, x, y_calc)
 max_rad = max(eps) + 30
@@ -54,5 +58,3 @@ for i in range(N):
     print([(intervals[i][0] + intervals[i][1]) / 2, (intervals[i][1] - intervals[i][0]) / 2])
 
 plotIntervals(intervals, x, y_calc)
-
-
